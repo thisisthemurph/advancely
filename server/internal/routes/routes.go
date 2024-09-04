4e634b9f5022629425f3b9983674441d558369ff
Mike Murphy <michael.w.murphy@outlook.com>
2024-09-04 06:58:52 +0100

    Improves route configuration

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -1,14 +1,13 @@
 package routes
 
 import (
-	"net/http"
-
 	"advancely/internal/application"
 	mw "advancely/internal/middleware"
 	"advancely/internal/validation"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 // RouteMaker is the interface used to specify handlers must implement the MakeRoutes method.
@@ -19,19 +18,7 @@ type RouteMaker interface {
 func NewRouter(app *application.App) *echo.Echo {
 	e := echo.New()
 	e.Validator = validation.NewCustomValidator()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{app.Config.ClientBaseURL},
-		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-		AllowMethods:     []string{http.MethodGet, http.MethodPost},
-		AllowCredentials: true,
-	}))
-
-	userMw := mw.NewUserMiddleware(app.Config.SessionSecret, app.Supabase, app.Store.UserStore, app.Logger)
-	e.Use(userMw.WithUserInContext)
+	setUpMiddlewares(e, app)
 
 	baseGroup := e.Group("/api/v1")
 	for _, h := range buildAPIHandlers(app) {
@@ -45,4 +32,21 @@ func buildAPIHandlers(app *application.App) []RouteMaker {
 	return []RouteMaker{
 		NewAuthHandler(app.Supabase, app.Store, app.Config.SessionSecret, app.Logger),
 	}
+}
+
+func setUpMiddlewares(e *echo.Echo, app *application.App) {
+	if app.IsDevelopment() {
+		e.Use(middleware.Logger())
+	}
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{app.Config.ClientBaseURL},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete},
+		AllowCredentials: true,
+	}))
+
+	userMw := mw.NewUserMiddleware(app.Config.SessionSecret, app.Supabase, app.Store.UserStore, app.Logger)
+	e.Use(userMw.WithUserInContext)
 }
