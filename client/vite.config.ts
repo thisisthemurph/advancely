e0b353ea390b60ed956233f0081963df165bec13
Mike Murphy <michael.w.murphy@outlook.com>
2024-09-07 11:45:59 +0100

    update vite config for different mode .env location

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,39 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import {defineConfig, loadEnv} from "vite";
+import { resolve } from "path"
+
+export default defineConfig(({ mode }) => {
+  const envDir = mode == "production"
+    ? resolve(__dirname, "..")
+    : process.cwd();
+
+  const env = loadEnv(mode, envDir, "");
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+      css: false,
+    },
+  }
+});
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    css: false,
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "./src/test/setup.ts",
+//     css: false,
+//   },
+// });
