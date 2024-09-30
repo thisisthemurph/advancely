896f34abcf5414c517a97bcfba0f0e81e66567ca
Mike Murphy <michael.w.murphy@outlook.com>
2024-09-30 06:47:33 +0100

    Adds a c# like List functionality. Just having fun.

--- server/pkg/fn/fn.go
+++ server/pkg/fn/fn.go
@@ -1,9 +1,9 @@
 package fn
 
-type mapFunc[E any] func(e E) E
+type MapFunc[E any] func(e E) E
 
 // Map applies the given function to each item of the slice and returns a slice of the results.
-func Map[S ~[]E, E any](s S, f mapFunc[E]) S {
+func Map[S ~[]E, E any](s S, f MapFunc[E]) S {
 	result := make(S, len(s))
 	for i := range s {
 		result[i] = f(s[i])
@@ -13,7 +13,7 @@ func Map[S ~[]E, E any](s S, f mapFunc[E]) S {
 
 type keepFunc[E any] func(e E) bool
 
-// Filter filters the iterable by applying the given function to each item, returning only the items for which the function returns true.
+// Filter filters the iterable by applying the given function to each item, returning only the elements for which the function returns true.
 func Filter[S ~[]E, E any](s S, f keepFunc[E]) S {
 	result := S{}
 	for _, v := range s {
@@ -26,7 +26,7 @@ func Filter[S ~[]E, E any](s S, f keepFunc[E]) S {
 
 type reduceFunc[E any] func(cur, next E) E
 
-// Reduce applies the given function cumulatively to the items of the slice, from left to right, to reduce the slice to a single value.
+// Reduce applies the given function cumulatively to the elements of the slice, from left to right, to reduce the slice to a single value.
 func Reduce[E any](s []E, initialValue E, f reduceFunc[E]) E {
 	result := initialValue
 	for _, v := range s {
